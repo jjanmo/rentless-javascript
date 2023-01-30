d0add85ed4292d7a4a0b90db71a7df26980bc0eb
jjanmo <jjanmo07@hanmail.net>
2023-01-30 23:53:48 +0900

    feat: define promise

---
+++ snippets/asynchronous/promise.js
@@ -0,0 +1,5 @@
+/**
+ * promise에 대한 여러가지 정의 중 와닿는 말들
+ * 1. A promise is an object that may produce a single value some time in the future
+ * 2. 내용은 실행되었지만 아직 결과를 반환하지 않은 객체 → 내가 원할때, 프로미스 객체를 통해서 값을 반환하여 조작할 수 있다
+ */
